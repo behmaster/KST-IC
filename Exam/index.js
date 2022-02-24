50c3ddf1b263715d4de03552c08347f95dba53b3
Robbie Behm <behmaster@gmail.com>
2022-02-23 20:16:24 -0600

    Intro Course Exam

---
+++ Exam/index.js
@@ -0,0 +1,20 @@
+// Obtain the img element, and assign it to the image variable
+let image = document.querySelector("#pet");
+console.log(image);
+
+// Obtain the 'shrink-grow' button, and assign it to the changeSize variable
+let changeSize = document.querySelector("button");
+// document.body.append(changeSize);
+console.log(changeSize);
+
+//DO NOT CHANGE THE FOLLOWING CODE
+changeSize.addEventListener("click", function () {
+  if (image.style.height == "100px") {
+    image.style.height = "25px";
+  } else {
+    image.style.height = "100px";
+  }
+});
+
+document.body.append(image);
+// getElementById
